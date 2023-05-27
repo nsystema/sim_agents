b493cdd87998db515e8483c762ce46415f1d56bf
nsystema <91890581+nsystema@users.noreply.github.com>
2023-05-27 19:42:22 +0000

    pkl

--- data_preprocess.py
+++ data_preprocess.py
@@ -250,16 +250,5 @@ def create_infos_from_protos(raw_data_path, output_path, num_workers=16):
         pickle.dump(val_infos, f)
     print('----------------Waymo info val file is saved to %s----------------' % val_filename)
     
-# create directory "senarios" if it does not exist
-if not os.path.exists('scenarios'):
-    os.makedirs('scenarios')
-# create directory "processed_scenarios_training" if it does not exist
-if not os.path.exists('processed_scenarios_training'):
-    os.makedirs('processed_scenarios_training')
-# create directory "processed_scenarios_validation" if it does not exist
-if not os.path.exists('processed_scenarios_validation'):
-    os.makedirs('processed_scenarios_validation')
-# create directory "processed_scenarios_testing" if it does not exist
-if not os.path.exists('processed_scenarios_testing'):
-    os.makedirs('processed_scenarios_testing')
-create_infos_from_protos('waymo_open_dataset_', 'scenarios')
+if __name__ == '__main__':
+    create_infos_from_protos('waymo_open_dataset_', 'scenarios')
